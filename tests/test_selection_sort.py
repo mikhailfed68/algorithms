2807d836f2385bab1371fb04c4f808fae148e7d0
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-07-04 18:25:18 +0300

    Add new test for selection_sort.py

--- tests/test_selection_sort.py
+++ tests/test_selection_sort.py
@@ -1,9 +1,15 @@
+import pytest
 from algorithms.selection_sort import find_smallest
 from algorithms.selection_sort import selection_sort
 
 
 def test_find_smallest():
     assert find_smallest([3, 5, 1, 6, 8, -1]) == 5
+
+
+def test_find_smallest_with_empty_arr():
+    with pytest.raises(IndexError):
+        find_smallest([]) == 0
 
 
 def test_selection_sort():
