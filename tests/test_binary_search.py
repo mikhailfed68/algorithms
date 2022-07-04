d4bbce78742995ec02dcec82c4afced6ba454353
Michael Fedorov <mikhailfedorov1939@gmail.com>
2022-07-04 13:06:08 +0300

    Refactoring test

--- tests/test_binary_search.py
+++ tests/test_binary_search.py
@@ -4,5 +4,6 @@ from algorithms.binary_search import binary_search
 def test_item():
     assert binary_search([5, 4, 3, 2, 1], 3) == 2
 
+
 def test_none():
     assert binary_search([5, 4, 3, 2, 1], 0) == None
